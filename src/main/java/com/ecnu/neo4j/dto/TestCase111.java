71891c33d52eca4b1d1d6a5598a08189c56ad0c5
lotus <9781490+hirinnLH@user.noreply.gitee.com>
2021-11-10 19:09:50 +0800

    新增需求11-1和12
    Signed-off-by: lotus <9781490+hirinnLH@user.noreply.gitee.com>

---
+++ src/main/java/com/ecnu/neo4j/dto/TestCase111.java
@@ -0,0 +1,10 @@
+package com.ecnu.neo4j.dto;
+
+import lombok.Data;
+
+@Data
+public class TestCase111 {
+    private int subwayStation;
+    private int initialStation;
+    private int endStation;
+}
---
+++ src/main/java/com/ecnu/neo4j/dto/TestCase12.java
@@ -0,0 +1,9 @@
+package com.ecnu.neo4j.dto;
+
+import lombok.Data;
+
+@Data
+public class TestCase12 {
+    private String type;
+    private int count;
+}
